156a39df09ca2d0e97001ea9ca953a38d7acc4f5
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2022-05-01 21:13:56 +0100

    top techs

--- src/components/ui/author-card/AuthorCard.tsx
+++ src/components/ui/author-card/AuthorCard.tsx
@@ -6,25 +6,31 @@ import VSCodeIcon from "../../../assets/svgs/icons8-visual-studio-code-2019 1.sv
 
 const AuthorCard = () => {
   return (
-    <div className="author_card_container">
-      <div className="author_card_img">
-        <div className="icon_border">
-          <img src={ReactIcon} alt="react icon" className="react_icon" />
-        </div>
-        <div className="javascript_icon_border">
-          <img
-            src={JavaScriptIcon}
-            alt="javascript icon"
-            className="javascript_icon"
-          />
-        </div>
-        <img src={MyAvatar} alt="alvin okoro" />
+    <div className="author_card">
+      <div className="author_techs_box">
+        <button className="author_techs reactjs_box">ReactJs</button>
+        <button className="author_techs javascript_box">TypeScript</button>
       </div>
-      <div className="author_details">
-        <h2 className="author_name">Alvin Okoro</h2>
-        <h3 className="author_stack">Software Engineer</h3>
-        <div className="icon_border">
-          <img src={VSCodeIcon} alt="vscode icon" className="vscode_icon" />
+      <div className="author_card_container">
+        <div className="author_card_img">
+          <div className="icon_border">
+            <img src={ReactIcon} alt="react icon" className="react_icon" />
+          </div>
+          <div className="javascript_icon_border">
+            <img
+              src={JavaScriptIcon}
+              alt="javascript icon"
+              className="javascript_icon"
+            />
+          </div>
+          <img src={MyAvatar} alt="alvin okoro" />
+        </div>
+        <div className="author_details">
+          <h2 className="author_name">Alvin Okoro</h2>
+          <h3 className="author_stack">Software Engineer</h3>
+          <div className="icon_border">
+            <img src={VSCodeIcon} alt="vscode icon" className="vscode_icon" />
+          </div>
         </div>
       </div>
     </div>
