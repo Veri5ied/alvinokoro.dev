2eba9ce33d4fb33e87f0724c73462fe58d2d4706
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2022-05-01 18:29:08 +0100

    top navigation logo

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import "./App.scss";
 import { SamplePage, NotFoundPage } from "./pages";
 import { Routes, Route } from "react-router-dom";
 import { INDEX_PAGE, NOT_FOUND_PAGE } from "./routes";
+import TopNavigation from "./components/ui/top-navigation/TopNavigation";
 
 function App() {
   return (
     <div className="App">
+      <TopNavigation />
       <Routes>
         <Route path={INDEX_PAGE} element={<SamplePage />} />
         <Route path={NOT_FOUND_PAGE} element={<NotFoundPage />} />
--- src/components/ui/top-navigation/TopNavigation.tsx
+++ src/components/ui/top-navigation/TopNavigation.tsx
@@ -1,9 +1,14 @@
-import "./style.scss"
+import "./style.scss";
 
 const TopNavigation = () => {
-    return (
-        <div></div>
-    )
-}
+  return (
+    <div className="container top_navigation">
+      <div className="top_navigation_left">
+        <h1 className="top_navigation_title">Alvin Okoro</h1>
+      </div>
+      <div className="top_navigation_right"></div>
+    </div>
+  );
+};
 
 export default TopNavigation;
