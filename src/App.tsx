6f6255a05576a2a8edbceeefa9f046bd53b93d48
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2022-05-04 12:38:31 +0100

    loading animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,34 @@
+import { useState, useEffect } from "react";
 import "./App.scss";
 import { MainPage, NotFoundPage } from "./pages";
 import { Routes, Route } from "react-router-dom";
 import { INDEX_PAGE, NOT_FOUND_PAGE } from "./routes";
 import TopNavigation from "./components/ui/top-navigation/TopNavigation";
 import Footer from "./components/ui/footer/Footer";
+import Loading from "./components/loading/Loading";
 
 function App() {
+  const [loading, setLoading] = useState<boolean>(true);
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 4000);
+  });
+
   return (
     <div className="portfolio_container">
-      <div className="container">
-        <TopNavigation />
-        <Routes>
-          <Route path={INDEX_PAGE} element={<MainPage />} />
-          <Route path={NOT_FOUND_PAGE} element={<NotFoundPage />} />
-        </Routes>
-      </div>
-      <Footer />
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <div className="container">
+            <TopNavigation />
+            <Routes>
+              <Route path={INDEX_PAGE} element={<MainPage />} />
+              <Route path={NOT_FOUND_PAGE} element={<NotFoundPage />} />
+            </Routes>
+          </div>
+          <Footer />
+        </>
+      )}
     </div>
   );
 }
