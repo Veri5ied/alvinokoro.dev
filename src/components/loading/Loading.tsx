cdb6143b837a747664d3e3502a033c626cad68de
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2022-05-04 12:24:14 +0100

    loading component

---
+++ src/components/loading/Loading.tsx
@@ -0,0 +1,13 @@
+import "./style.scss";
+
+const Loading = () => {
+  return (
+    <div className="loading_container">
+      <div className="bar"></div>
+      <div className="bar"></div>
+      <div className="bar"></div>
+    </div>
+  );
+};
+
+export default Loading;
